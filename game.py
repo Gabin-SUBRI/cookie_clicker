461aeb16744a8ba4cb6b612d5210ee4964303d08
Gabin Subri <gabin.subri@edu.igensia.com>
2025-04-03 19:21:54 +0200

    jeu finis

--- game.py
+++ game.py
@@ -1,75 +1,116 @@
+#Imports of this program
 import msvcrt
+import random
 import time
-import threading
 
-
-# Variable
+#Variables
 compteur = 0
 touche = ""
 amélio = 1
 achat_ajoute1 = 0
 achat_ajoute2 = 0
-gain_par_seconde = 0
+achat_ajoute3 = 0
+achat_ajoute4 = 0
+objectif = 1000000000
 
-# Dictionnaire des améliorations
-ameliorations = {
-    "Ajoute 1 a tes clicks" : 1,
-}
-
-
-#Boutique d'amélioration
-
-def boutique(compteur, amélio, achat_ajoute1, achat_ajoute2, gain_par_seconde):
+#Shop
+def boutique(compteur, amélio, achat_ajoute1, achat_ajoute2,achat_ajoute3,achat_ajoute4):
     print("\nBienvenue à la boutique !")
     print(f"Tu as {compteur} cookies.")    
-    print(f"1. Amélioration de vitesse (+1) : {(achat_ajoute1 +1) *50} cookies (Acheté {achat_ajoute1} fois)")
-    print(f"2. Amélioration de gain automatique : {(achat_ajoute2 + 1) * 200} cookies (Acheté {achat_ajoute2} fois)")
+    print(f"1. Amélioration de click (+1) : {(achat_ajoute1 +1) *50 *(achat_ajoute1 + 1)} cookies (Acheté {achat_ajoute1} fois)")
+    print(f"2. Amélioration de click (*2) : {(achat_ajoute2 + 1) * 200* (achat_ajoute2+1)} cookies (Acheté {achat_ajoute2} fois)")
+    print(f"3. Augmente ta chance d'avoir un cookie bonus : {(achat_ajoute3 + 1) * 1000 *(achat_ajoute3 + 1)} cookies (Acheté {achat_ajoute3} fois) ")
+    print(f"4. Augmente ta quantité de cookie bonus : {(achat_ajoute4 + 1) * 2000*(achat_ajoute4 + 1)} cookies (Acheté {achat_ajoute4} fois) ")
     print("Appuie sur 'q' pour quitter la boutique.")
 
+#Game loop for shop
     while True:
         choix = msvcrt.getch().decode("utf-8")
-        if choix == "1" and compteur >= 50:
+        #choix 1
+        if choix == "1" and compteur >= (achat_ajoute1 +1) *50 *(achat_ajoute1 + 1):
             amélio += 1
-            compteur -= 50
+            compteur -= (achat_ajoute1 +1) *50 *(achat_ajoute1 + 1)
             achat_ajoute1 += 1
-            print("Amélioration de vitesse achetée ! 🚀")
-        elif choix == "2" and compteur >= 200:
-            gain_par_seconde += 0.5
-            def auto_gain():
-                global compteur
-                while True:
-                    compteur += gain_par_seconde
-                    time.sleep(1)
-
-                    if not hasattr(boutique, "thread_started"):
-                        thread = threading.Thread(target=auto_gain, daemon=True)
-                        thread.start()
-                        boutique.thread_started = True
-                        
-            thread = threading.Thread(target=auto_gain, daemon=True)
-            thread.start()
-            compteur -= 200
+            print(f"Amélioration de click achetée ! 🚀 {achat_ajoute1}")
+            #choix 2
+        elif choix == "2" and compteur >= (achat_ajoute2 + 1) * 200* (achat_ajoute2+1):
+            amélio *= 2
+            compteur -= (achat_ajoute2 + 1) * 200* (achat_ajoute2+1)
             achat_ajoute2 += 1
-
-            
-            print(f"Bravo ! Tu es a {gain_par_seconde} cookies/secondes ✨")
+            print(f"Amélioration de click achetée ! 🚀 {achat_ajoute2}")
+        elif choix == "3" and compteur >= (achat_ajoute3 +1) * 1000 *(achat_ajoute3 + 1):
+            compteur -= (achat_ajoute3 +1) *1000*(achat_ajoute3 + 1)
+            achat_ajoute3 += 1
+            print(f"Amélioration de chance élevé à {achat_ajoute3}%")
+        elif choix == "4" and compteur >= (achat_ajoute4 +1) * 2000*(achat_ajoute4 + 1):
+            compteur -= (achat_ajoute4 +1) *2000*(achat_ajoute4 + 1)
+            achat_ajoute4 += 1
+            print(f"Amélioration de cookie bonus à x{achat_ajoute4+2}")
         elif choix == "q":
             print("Merci pour ta visite !")
             break
         else:
             print("Option invalide ou pas assez de cookies.")
-    return compteur, amélio, achat_ajoute1, achat_ajoute2, gain_par_seconde
+    return compteur, amélio, achat_ajoute1, achat_ajoute2, achat_ajoute3, achat_ajoute4
 
-
-print("Appuie sur 'Espace' pour miner des cookies. \nAppuie sur 'b' pour accéder à la boutique.\nAppuie sur 'q' pour quitter.")
-
+#Menu
+print("Appuie sur 'Espace' pour miner des cookies. \n"
+    "Appuie sur 'b' pour accéder à la boutique.\n"
+    "Appuie sur 'q' pour quitter.")
+start_time = time.time() # demarre le chronometre
+#Available controls
 while touche != "q":
     touche = msvcrt.getch().decode("utf-8")
-
     if touche == " ":
+        if random.randint(1,100) <= achat_ajoute3 :
+            compteur += amélio * (achat_ajoute4 + 1)
+            print(f"Cookies bonus x{achat_ajoute4+1} !!")
         compteur += amélio
         print(f"Cookies : {compteur}")
-
     if touche == "b":
-        compteur, amélio, achat_ajoute1, achat_ajoute2, gain_par_seconde  = boutique(compteur,amélio,achat_ajoute1,achat_ajoute2,gain_par_seconde)
+        compteur,amélio,achat_ajoute1,achat_ajoute2,achat_ajoute3, achat_ajoute4 = boutique(compteur,amélio,achat_ajoute1,achat_ajoute2, achat_ajoute3, achat_ajoute4)
+    if touche == "o":
+        compteur += 1000000000
+    if touche == "p":        
+        elapsed_time = time.time() - start_time  # Temps écoulé en secondes
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print(f"{minutes}min {seconds}s")
+    if compteur >= objectif:
+        print("\n🎉 Félicitations ! Tu as atteint l'objectif de 1 000 000 000 cookies ! 🎉")
+        print (f"Tu as améilorer {achat_ajoute1} fois l'amélioration 1")
+        print (f"Tu as améilorer {achat_ajoute2} fois l'amélioration 2")
+        print (f"Tu as améilorer {achat_ajoute3} fois l'amélioration 3")
+        print (f"Tu as améilorer {achat_ajoute4} fois l'amélioration 4")
+        elapsed_time = time.time() - start_time  # Temps écoulé en secondes
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print(f"Temps total de jeu : {minutes} min {seconds} sec")
+        print("Tu peux relancer le jeu pour essayer de battre ton score.")
+        print ("quelquun l'a fait en 1min et 12s")
+        break 
+
 print("Fin du programme.")
+
+#                                  .:=+*##%%%%%%**+=-:.                :-+**##%%%%%%%%**++=-:.                       
+#                               -*%@@@@@@@@@@@@@@@@@@@@%*.         .=#@@@@@@@@@@@@@@@@@@@@@@@@%=                     
+#                            .+%@@@@@@@@@@@@@@@@@@@@@@@@@=        =@@@@@@@@@@@@@@@@@@@@@@@@@@@@#                     
+#                          .*@@@@@@@@@@@@@@@@@@@@@@@@@@@@-       *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%                     
+#                         -%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.      :@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                    
+#                        =@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%       =@@@@@@@@@@@@@@@%*++++*#%@@@@@@@:                    
+#                       -@@@@@@@@@@@@@@@%+-:...:-=*%@@@@*       -@@@@@@@@@@@@@+           :=*@@@:                    
+#                     .@@@@@@@@@@@@@@%-            .=#@-        #@@@@@@@@@@@@=               -=                     
+#                      +@@@@@@@@@@@@@%.                          .*@@@@@@@@@@@@%*=:.                                 
+#                      %@@@@@@@@@@@@@-        =****####%%%%%:      :+%@@@@@@@@@@@@@@@%*+-.                           
+#                     :@@@@@@@@@@@@@%         *@@@@@@@@@@@@@+         :=*%@@@@@@@@@@@@@@@@@#+:                       
+#                     :@@@@@@@@@@@@@%         .@@@@@@@@@@@@@*             .:=+#%@@@@@@@@@@@@@@%=.                    
+#                     :@@@@@@@@@@@@@@          @@@@@@@@@@@@@#  .:.              .=#@@@@@@@@@@@@@%:                   
+#                     .@@@@@@@@@@@@@@=        -@@@@@@@@@@@@@%  #@@+:               %@@@@@@@@@@@@@%.                  
+#                      #@@@@@@@@@@@@@@+.    .+@@@@@@@@@@@@@@@  #@@@@%*-:          -@@@@@@@@@@@@@@@=                  
+#                      -@@@@@@@@@@@@@@@@%##%@@@@@@@@@@@@@@@@@  #@@@@@@@@@%%*****#@@@@@@@@@@@@@@@@@=                  
+#                       #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@. #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:                  
+#                       .#@@@@@@@@@@@@@@@@@@@@@#-@@@@@@@@@@@@. *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+                   
+#                         +@@@@@@@@@@@@@@@@@@@#..@@@@@@@@@@@@. *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%-                    
+#                          :*@@@@@@@@@@@@@@@@+  :@@@@@@@@@@@@. -%@@@@@@@@@@@@@@@@@@@@@@@@@@@@#=                      
+#                            .-+#%@@@@@@@@#=.   :%@@@@@@@@@@%.   .:-=+*#%%@@@@@@@@@@@@@%%*=-.                        
+#                                  .::::.           ......                  ..::::::..                               
